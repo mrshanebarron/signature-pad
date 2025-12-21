dcb12a03e58f0100fa014b4f7c5837e39517fc1a
Shane Barron <shane@Shanes-MacBook-Pro.local>
2025-12-20 23:33:10 -0500

    Add missing public properties to Livewire component

--- src/Livewire/SignaturePad.php
+++ src/Livewire/SignaturePad.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class SignaturePad extends Component
 {
+    public int $width = 400;
+    public int $height = 200;
+    public string $strokeColor = '#000000';
+    public int $strokeWidth = 2;
+    public string $backgroundColor = '#ffffff';
+    public ?string $signature = null;
+
     public function render()
     {
         return view('sb-signature-pad::livewire.signature-pad');
